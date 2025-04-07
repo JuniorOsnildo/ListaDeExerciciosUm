f3a3a4865722e1c8ec437216faa6f6f73353e392
Osnildo <juniorosnildo777@gmail.com>
2025-04-07 05:46:40 -0300

    pronto

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,17 @@
+﻿static float VolumeRetangulo(float a, float b, float c)
+{
+    return a * b * c;
+}
+
+Console.WriteLine("digite o primeiro valor");
+float a = float.Parse(Console.ReadLine());
+
+Console.WriteLine("digite o segundo valor");
+float b = float.Parse(Console.ReadLine());
+
+Console.WriteLine("digite o terceiro valor");
+float c = float.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine("O volume do retangulo é de: "+ VolumeRetangulo(a, b, c));
